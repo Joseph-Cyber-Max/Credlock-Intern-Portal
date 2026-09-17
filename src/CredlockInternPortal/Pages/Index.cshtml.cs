520952ab5e9af07ffc429feb4308c13bfa6e9812
Joseph <116102439+Joseph-Cyber-Max@users.noreply.github.com>
2026-09-17 19:11:22 +0100

    fix: route root page through authentication

--- src/CredlockInternPortal/Pages/Index.cshtml.cs
+++ src/CredlockInternPortal/Pages/Index.cshtml.cs
@@ -1,10 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CredlockInternPortal.Pages;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
+    public IActionResult OnGet()
     {
+        return HttpContext.Session.GetString("UserId") is null
+            ? RedirectToPage("/Login")
+            : RedirectToPage("/Dashboard/Index");
     }
 }
