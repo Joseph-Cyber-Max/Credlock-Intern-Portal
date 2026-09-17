e465c51ed2b96f6ff3d6f65eb84368c7914c3654
Joseph <116102439+Joseph-Cyber-Max@users.noreply.github.com>
2026-09-17 18:49:16 +0100

    Configure authentication services and session

--- src/CredlockInternPortal/Program.cs
+++ src/CredlockInternPortal/Program.cs
@@ -1,6 +1,18 @@
+using CredlockInternPortal.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Dashboard");
+});
+builder.Services.AddSingleton<AuthService>();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromHours(8);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -13,6 +25,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseSession();
 
 app.MapRazorPages();
 
